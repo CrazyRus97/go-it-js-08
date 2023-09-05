8ac09872ba84db23e8948a3443c74293b0f7d13c
Fedorenko Ruslan <130165394+CrazyRus97@users.noreply.github.com>
2023-09-05 20:02:37 +0300

    task-2 finall

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,6 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
@@ -5,7 +8,7 @@ const onPlay = function (data) {
     localStorage.setItem('videoplayer-current-time', data.seconds);
 };
 
-player.on('timeupdate', _.throttle(onPlay, 1000));
+player.on('timeupdate', throttle(onPlay, 1000));
 
 const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
 
